d3b5c98bdf8115a9398a296045da961a4c201ec9
shlyyzy <shirleyy0103@gmail.com>
2021-03-31 20:58:09 -0600

    Added functionality to grab Twitter username data from API

---
+++ api.pl
@@ -0,0 +1,50 @@
+:- module(twitter,
+         [token/1,
+          search/4]).
+
+:- use_module(library(http/thread_httpd)).
+:- use_module(library(http/http_dispatch)).
+:- use_module(library(http/http_error)).
+:- use_module(library(http/html_write)).
+:- use_module(library(http/http_session)).
+:- use_module(library(http/js_write)).
+:- use_module(library(http/http_files)).
+:- use_module(library(http/json)).
+:- use_module(library(http/http_open)).
+:- use_module(library(http/http_json)).
+:- use_module(library(http/http_parameters)).
+:- use_module(library(http/http_client)).
+:- use_module(library(http/http_ssl_plugin)).
+
+:- dynamic
+	token/1.
+
+% examples
+% search('walrus', RecommendedUsers, ErrorCode, Num).
+% search('(grumpy cat OR cat) OR (#meme has:images)', RecommendedUsers, ErrorCode, Num).
+
+get_bearer_token(T) :- getenv("TWITTER_BEARER_TOKEN", T).
+
+search(My_Search,RecommendedUsers,ErrorCode, M):-
+	get_bearer_token(B_Token64),
+	Path='/1.1/search/tweets.json',
+	Search=[q(My_Search), result_type='popular'],
+	get_response(Path, Search, B_Token64, RecommendedUsers, ErrorCode, M).
+
+get_response(Path, Search, B_Token64, RecommendedUsers, ErrorCode, M) :-
+	URL=[scheme(https), host('api.twitter.com'), path(Path), search(Search)],
+	Options=[	authorization(bearer(B_Token64)),
+				status_code(ErrorCode)
+			],
+	setup_call_cleanup(http_open(URL, In, Options),
+					   json_read_dict(In, JSON),
+					   close(In)),
+	is_dict(JSON),
+	get_screen_names(JSON.statuses, RecommendedUsers, M).
+
+get_screen_names([], [], 0).
+get_screen_names([H|T], R, M) :- get_screen_names(T, R, N), member(H.user.screen_name, R), M is N+1.
+get_screen_names([H|T], [H.user.screen_name | R], M) :- get_screen_names(T, R, N), \+ member(H.user.screen_name, R), M is N+1.
+
+% get_screen_names([H|T], R, M) :- get_screen_names(T, R, N), member(H, R), M is N+1.
+% get_screen_names([H|T], [H | R], M) :- get_screen_names(T, R, N), \+ member(H, R), M is N+1.
--- dictionary.pl
+++ dictionary.pl
@@ -15,32 +15,72 @@ adjectives(L0,L2,Entity,C0,C2) :-
 adjectives(L,L,_,C,C).
 */
 
+phrase([], [], _, C,C).
+% phrase(L0, L5, E1, C0, C5) :-
+%   keyword(L0, L1, E1, C0, C1),
+%   has_option(L1, L2, E1, C1, C2),
+%   is_option(L2, L3, E1, C2, C3),
+%   operator(L3, L4, E1, C3, C4),
+%   phrase(L4, L5, E1, C4, C5).
+
 phrase(L0, L5, E1, C0, C5) :-
   keyword(L0, L1, E1, C0, C1),
   has_option(L1, L2, E1, C1, C2),
-  is_option(L2, L3, E1, C2, C3),
-  operator(L3, L4, E1, C3, C4),
-  phrase(L4, L5, E1, C4, C5).
+  phrase(L2, L5, E1, C1, C5).
+phrase(L0, L5, E1, C0, C5) :-
+  keyword(L0, L1, E1, C0, C1),
+  is_option(L1, L2, E1, C1, C2),
+  phrase(L2, L5, E1, C1, C5).
+phrase(L0, L5, E1, C0, C4) :-
+  keyword(L0, L1, E1, C0, C1),
+  operator(L1, L2, E1, C1, C2),
+  keyword(L2, L3, E1, C2, C3),
+  phrase(L3, L5, E1, C3, C4).
+phrase(L0, L5, E1, C0, C4) :-
+  has_option(L0, L1, E1, C0, C1),
+  operator(L1, L2, E1, C1, C2),
+  keyword(L2, L3, E1, C2, C3),
+  phrase(L3, L5, E1, C3, C4).
+phrase(L0, L5, E1, C0, C4) :-
+  is_option(L0, L1, E1, C0, C1),
+  operator(L1, L2, E1, C1, C2),
+  keyword(L2, L3, E1, C2, C3),
+  phrase(L3, L5, E1, C3, C4).
+phrase(L0, L5, E1, C0, C5) :-
+  keyword(L0, L1, E1, C0, C1),
+  phrase(L1, L5, E1, C1, C5).
+
+phrase(L0, L5, E1, C0, C5) :-
+  has_option(L0, L1, E1, C0, C1),
+  phrase(L1, L5, E1, C1, C5).
+phrase(L0, L5, E1, C0, C5) :-
+  is_option(L0, L1, E1, C0, C1),
+  phrase(L1, L5, E1, C1, C5).
+% phrase(L0, L5, E1, C0, C5) :-
+%   operator(L0, L1, E1, C0, C1),
+%   phrase(L1, L5, E1, C1, C5).
 phrase([L0], L, E1, C0, C1) :- keyword([L0], L, E1, C0, C1).
 phrase([L0], L, E1, C0, C1) :- has_option([L0], L, E1, C1, C2).
 phrase([L0], L, E1, C0, C1) :- is_option([L0], L, E1, C2, C3).
-phrase([], [], _, C,C).
+
 
 % adj([large | L],L,Entity, [large(Entity)|C],C).
 % has_option([media | L], L, Entity, [large(Entity)|C],C).
 % has_option([links | L], ...)
 % has_option([images | L], ...)
 
-operator([X | L], L, _, C, C) :- op(X).
-operator([L], [], _, C, C) :- op(L).
 operator([], [], _, C, C).
+operator([X | L], L, _, C, C) :- op(X).
+% operator([L], [], _, C, C) :- op(L).
 
-has_option([X | L],L,Y,C,C) :- has(X).
-has_option([L], [], _, C, C) :- has(L).
-has_option([], [], _, C, C).
+is_option([], [], _, C, C).
 is_option([X | L],L,Y,C,C) :- is(X).
 is_option([L], [], _, C, C) :- is(L).
-is_option([], [], _, C, C).
+
+has_option([], [], _, C, C).
+has_option([X | L],L,Y,C,C) :- has(X).
+has_option([L], [], _, C, C) :- has(L).
+
 % is_option([X | L],L,X, C,D) :- is(X).
 
 % has_option([X | L], L) :- has(X).
@@ -50,7 +90,7 @@ is_option([], [], _, C, C).
 
 % has_option([L0 | L1], L1, E1, [has(L0) | C1], C1) :- has(L0).
 
-
+% keyword([], [], _, C, C).
 keyword([L0 | L1], L1, E1, C0, C1) :-
   \+ operator([L0 | L1], L1, E1, C0, C1),
   \+ has_option([L0 | L1], L1, E1, C0, C1),
@@ -59,7 +99,7 @@ keyword([L], L1, _, C, C) :-
   \+ operator([L], L1, E1, C0, C1),
   \+ has_option([L], L1, E1, C0, C1),
   \+ is_option([L], L1, E1, C0, C1).
-keyword([], [], _, C, C).
+
 
 has(media).
 has(links).
